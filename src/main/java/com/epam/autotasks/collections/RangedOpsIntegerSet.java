69ab44f222db9fd512161b13cb8ad1b7a0d9955d
Yevhen Sikora <risenseraph00@gmail.com>
2022-09-13 21:46:51 +0300

    Module 3 Task 1

--- src/main/java/com/epam/autotasks/collections/RangedOpsIntegerSet.java
+++ src/main/java/com/epam/autotasks/collections/RangedOpsIntegerSet.java
@@ -1,35 +1,52 @@
 package com.epam.autotasks.collections;
 
-import java.util.AbstractSet;
-import java.util.Iterator;
+import java.util.*;
 
 class RangedOpsIntegerSet extends AbstractSet<Integer> {
+    private TreeMap<Integer, Object> map;
+    private static final Object PRESENT = new Object();
+
+    public RangedOpsIntegerSet() {
+        map = new TreeMap<>();
+    }
 
     public boolean add(int fromInclusive, int toExclusive) {
-        throw new UnsupportedOperationException();
+        boolean actuallyAdded = false;
+        for (int i = fromInclusive; i < toExclusive; i++ ) {
+            if (add(i)) {
+                actuallyAdded = true;
+            }
+        }
+        return actuallyAdded;
     }
 
     public boolean remove(int fromInclusive, int toExclusive) {
-        throw new UnsupportedOperationException();
+        boolean actuallyRemoved = false;
+        for (int i = fromInclusive; i < toExclusive; i++ ) {
+            if (remove(i)) {
+                actuallyRemoved = true;
+            }
+        }
+        return actuallyRemoved;
     }
 
     @Override
     public boolean add(final Integer integer) {
-        throw new UnsupportedOperationException();
+       return map.put(integer, PRESENT) == null;
     }
 
     @Override
     public boolean remove(final Object o) {
-        throw new UnsupportedOperationException();
+        return map.remove(o, PRESENT);
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        throw new UnsupportedOperationException();
+        return map.keySet().iterator();
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return map.size();
     }
 }
